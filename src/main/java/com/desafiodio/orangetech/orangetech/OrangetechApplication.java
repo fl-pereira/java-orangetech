c679510f7faf44619d123915f8091bdc8e31d4bb
fl-pereira <fpereira.media@gmail.com>
2023-01-19 16:45:55 -0300

    first commit

---
+++ src/main/java/com/desafiodio/orangetech/orangetech/OrangetechApplication.java
@@ -0,0 +1,13 @@
+package com.desafiodio.orangetech.orangetech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangetechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangetechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafiodio/orangetech/orangetech/OrangetechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiodio.orangetech.orangetech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangetechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
