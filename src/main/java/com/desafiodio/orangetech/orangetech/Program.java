64e355d9c47f50da16306e34a3d61a73eb1d97c4
fl-pereira <fpereira.media@gmail.com>
2023-01-19 16:47:57 -0300

    sync

--- src/main/java/com/desafiodio/orangetech/orangetech/Program.java
+++ src/main/java/com/desafiodio/orangetech/orangetech/Program.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrangetechApplication {
+public class Program {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrangetechApplication.class, args);
+		SpringApplication.run(Program.class, args);
 	}
 
 }
