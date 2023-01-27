5dd933a7ac075b7e2b070ae846edff96682f233b
fl-pereira <fpereira.media@gmail.com>
2023-01-27 15:15:49 -0300

    final

---
+++ src/main/java/com/felipepereira/springboot/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.felipepereira.springboot.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+
+}
